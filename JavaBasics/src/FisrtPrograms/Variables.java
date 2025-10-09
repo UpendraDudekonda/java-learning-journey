f683c728293b965ab7e7fb29b119d30a1763716e
UpendraDudekonda <2124upendra@gmail.com>
2025-10-09 19:20:56 +0530

    variables declaration in java "var"

---
+++ JavaBasics/src/FisrtPrograms/Variables.java
@@ -0,0 +1,33 @@
+package FisrtPrograms;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		 //we can declare the variables in java by using var keyword
+		
+		var a = 10;
+		var b = 'A';
+		var c = "upendra";
+		
+		var _a = 99;
+		var $b = 15;
+		
+		// var 1ag = 20; // not valid
+		
+		var abc10 = 20;
+		
+		var abc_10 = 30;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(_a);
+		System.out.println($b);
+		System.out.println(abc10);
+		System.out.println(abc_10);
+		
+	}
+
+}
