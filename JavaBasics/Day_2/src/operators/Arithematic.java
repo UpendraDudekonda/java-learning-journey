d1f313806136b5dfcbae17307a1aa995142bb444
UpendraDudekonda <2124upendra@gmail.com>
2025-10-12 15:08:22 +0530

    operators

---
+++ JavaBasics/Day_2/src/operators/Arithematic.java
@@ -0,0 +1,26 @@
+package operators;
+
+public class Arithematic {
+
+	public static void main(String[] args) {
+		// +, - , * , / ,%
+		
+		var a = 20;
+		 var b= 5;
+		 
+		 System.out.println("Addition :" + (a+b));
+		 System.out.println("Subtraction :" + (a-b));
+		 System.out.println("Multiplication :" + (a*b));
+		 System.out.println("Division :" + (a/b));
+		 System.out.println("Modulus :" + (a%b));
+		 
+//		OUTPUT
+		 
+//		 Addition :25
+//		 Subtraction :15
+//		 Multiplication :100
+//		 Division :4
+//		 Modulus :0
+	}
+
+}
